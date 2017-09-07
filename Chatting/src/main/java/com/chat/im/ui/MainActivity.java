4f84f56dcda1c0140622b4902ea3f17fa8761ff0
唐利涛 <tanglt@sdc.icbc.com.cn>
2017-09-07 16:09:31 +0800

    替换图标

--- Chatting/src/main/java/com/chat/im/ui/MainActivity.java
+++ Chatting/src/main/java/com/chat/im/ui/MainActivity.java
@@ -133,8 +133,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 mImage_tabMe.setImageResource(R.drawable.tab_me_0);
 
                 mText_tabMessage.setTextColor(getResources().getColor(R.color.colorPrimary));
-                mText_tabContact.setTextColor(getResources().getColor(R.color.shallow_black));
-                mText_tabMe.setTextColor(getResources().getColor(R.color.shallow_black));
+                mText_tabContact.setTextColor(getResources().getColor(R.color.gray));
+                mText_tabMe.setTextColor(getResources().getColor(R.color.gray));
 
                 mViewPager.setCurrentItem(0);
                 break;
@@ -147,9 +147,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 mImage_tabContact.setImageResource(R.drawable.tab_contact_1);
                 mImage_tabMe.setImageResource(R.drawable.tab_me_0);
 
-                mText_tabMessage.setTextColor(getResources().getColor(R.color.shallow_black));
+                mText_tabMessage.setTextColor(getResources().getColor(R.color.gray));
                 mText_tabContact.setTextColor(getResources().getColor(R.color.colorPrimary));
-                mText_tabMe.setTextColor(getResources().getColor(R.color.shallow_black));
+                mText_tabMe.setTextColor(getResources().getColor(R.color.gray));
 
                 mViewPager.setCurrentItem(1);
                 break;
@@ -162,8 +162,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 mImage_tabContact.setImageResource(R.drawable.tab_contact_0);
                 mImage_tabMe.setImageResource(R.drawable.tab_me_1);
 
-                mText_tabMessage.setTextColor(getResources().getColor(R.color.shallow_black));
-                mText_tabContact.setTextColor(getResources().getColor(R.color.shallow_black));
+                mText_tabMessage.setTextColor(getResources().getColor(R.color.gray));
+                mText_tabContact.setTextColor(getResources().getColor(R.color.gray));
                 mText_tabMe.setTextColor(getResources().getColor(R.color.colorPrimary));
 
                 mViewPager.setCurrentItem(2);
