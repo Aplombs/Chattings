fce9f7e9da74dab139ebd1dbf811596ab69b6d2a
唐利涛 <tanglt@sdc.icbc.com.cn>
2017-09-11 16:07:57 +0800

    消息页签没有数据暂时处理:提示暂无新消息

--- Chatting/src/main/java/com/chat/im/fragment/MainTab_MessageFragment.java
+++ Chatting/src/main/java/com/chat/im/fragment/MainTab_MessageFragment.java
@@ -13,9 +13,17 @@ import com.chat.im.R;
 import com.chat.im.adapter.MessagePreViewAdapter;
 import com.chat.im.db.bean.MessagePreView;
 import com.chat.im.helper.ContextHelper;
+import com.chat.im.helper.DBHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * 消息页签
@@ -27,10 +35,22 @@ public class MainTab_MessageFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private List<MessagePreView> mMessagePreViewList = new ArrayList<>();
     private MessagePreViewAdapter mAdapter;
+    private View mNotMessageTip;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Observable.create(new ObservableOnSubscribe<List<MessagePreView>>() {
+            @Override
+            public void subscribe(ObservableEmitter<List<MessagePreView>> observableEmitter) {
+                mMessagePreViewList = DBHelper.getInstance().getDaoSession().getMessagePreViewDao().queryBuilder().list();
+            }
+        }).subscribeOn(Schedulers.io())//被观察者在子线程
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<List<MessagePreView>>() {
+            @Override
+            public void accept(List<MessagePreView> messagePreViewList) throws Exception {
+            }
+        });
     }
 
     @Nullable
@@ -44,10 +64,18 @@ public class MainTab_MessageFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        if (mMessagePreViewList.size() == 0) {
+            mNotMessageTip.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
+        } else {
+            mRecyclerView.setVisibility(View.VISIBLE);
+            mNotMessageTip.setVisibility(View.GONE);
+        }
     }
 
     private void initView() {
         mRecyclerView = (RecyclerView) mView.findViewById(R.id.recyclerView_tabMessage);
+        mNotMessageTip = mView.findViewById(R.id.ll_not_message_tip);
         mAdapter = new MessagePreViewAdapter(mMessagePreViewList);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(ContextHelper.getContext()));
         mRecyclerView.setAdapter(mAdapter);
